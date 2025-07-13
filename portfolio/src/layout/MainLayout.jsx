6c812d2b549d03d17412520a12e596e3f5ff64cc
ayman samir <ayman_Samir_ahmed@hotmail.com>
2025-07-13 20:28:16 +0300

    prepare routes and main components

--- portfolio/src/layout/MainLayout.jsx
+++ portfolio/src/layout/MainLayout.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SharedLayout from '../sharedlayout/SharedLayout'
+import Home from '../pages/Home'
 
 export default function MainLayout() {
   return (
-    <div>MainLayout</div>
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<SharedLayout/>}>
+              <Route index element={<Home />}/>
+          </Route>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
