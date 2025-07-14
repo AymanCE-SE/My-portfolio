b044bd00e116d36002b234c927b6005058410eb4
ayman samir <ayman_Samir_ahmed@hotmail.com>
2025-07-14 18:52:44 +0300

    navbar V1

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import MainLayout from './layout/MainLayout'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./styles/global.css"
+import "bootstrap/dist/js/bootstrap.min.js"
 createRoot(document.getElementById('root')).render(
 <MainLayout></MainLayout>
 )
